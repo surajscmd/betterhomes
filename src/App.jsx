2a026b7238ad61375dd250cc33ec37a6ff2fa539
Suraj S <suraj.qwe777@gmail.com>
2024-07-05 18:04:47 +0530

    footter update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import Headsec from './elements/Headsec'
 import Ourprojexts from './elements/Ourprojexts'
 import Partner from './elements/Partner'
+import Footers from './elements/Footers'
 function App() {
 
   return (
     <>
+    <div className='page-container'>
       <Headsec/>
       <Partner/>
       <Ourprojexts/>
+      <Footers/>
+    </div>
+    
     </>
   )
 }
---
+++ src/elements/Footers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import mail from "../assets/envelope.svg"
+import call from "../assets/call.svg"
+import whatsapp from "../assets/whatsapp.svg" 
+const Footers = () => {
+  return (
+    <div className='footer-sec'>
+         <div className='button-container'>
+          <button className='btn-eta yellow'>  <img className='icon-eta' src={mail} alt="mail"/> Enquire</button>
+          <button className='btn-eta blue'>  <img className='icon-eta' src={call} alt="mail"/> Call</button>
+          <button className='btn-eta green'>  <img className='icon-eta' src={whatsapp} alt="mail"/> Whatsapp</button>
+         </div>
+    </div>
+  )
+}
+
+export default Footers
