b014599280184ac2192a1df40bf5d76b4e0d840f
Suraj S <suraj.qwe777@gmail.com>
2024-07-05 21:05:56 +0530

    call btn removed

--- src/elements/Footers.jsx
+++ src/elements/Footers.jsx
@@ -7,7 +7,8 @@ const Footers = () => {
     <div className='footer-sec'>
          <div className='button-container'>
           <button className='btn-eta yellow'>  <img className='icon-eta' src={mail} alt="mail"/> Enquire</button>
-          <button className='btn-eta blue'>  <img className='icon-eta' src={call} alt="mail"/> Call</button>
+      
+      
           <button className='btn-eta green'>  <img className='icon-eta' src={whatsapp} alt="mail"/> Whatsapp</button>
          </div>
     </div>
