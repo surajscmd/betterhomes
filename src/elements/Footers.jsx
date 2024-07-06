feaf14f63eb9a52effe1f9ba2c59c2e1e5349800
Suraj S <suraj.qwe777@gmail.com>
2024-07-06 22:25:42 +0530

    footer fixed

--- src/elements/Footers.jsx
+++ src/elements/Footers.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import mail from "../assets/envelope.svg"
 import call from "../assets/call.svg"
-import whatsapp from "../assets/whatsapp.svg" 
+
+
 const Footers = () => {
   return (
     <div className='footer-sec'>
-         <div className='button-container'>
-         
-         </div>
+      <div className='btn-section'>
+          <button className='btn-foot blue'><img className='icon-eta' src={call} alt="mail"/>Call</button>
+          <button className='btn-foot yellow'><img className='icon-eta' src={mail} alt="mail"/> Enquire</button>
+      </div>
+        
     </div>
   )
 }
